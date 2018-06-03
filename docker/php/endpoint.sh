ce312bd6cebdee95393812d690b797abdb65aa49
ffphoen1x <a.holubinets@gmail.com>
2018-06-03 23:14:16 +0300

    init

---
+++ docker/php/endpoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#cd /var/www/
+
+#if [ ! -d "vendor" ]; then
+#  composer -v install;
+#fi
+
+#cp /var/www/html/.docker/webapp/config/.env.default .env;
+
+#php artisan make:database;
+#php artisan migrate;
+#php artisan key:generate;
+
+#composer update;
+#composer dump-autoload;
